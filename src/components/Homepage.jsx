0afc7bc7819ffc2f9a861ef14021bcab564dd24b
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-02-25 23:16:35 +0400

    added global stats

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,7 +7,9 @@ const { Title } = Typography;
 export default function Homepage() {
 	const { data, isFetching } = useGetCryptosQuery();
 
-	console.log(data, isFetching);
+	const globalStats = data?.data?.stats;
+
+	if (isFetching) return "Loading";
 
 	return (
 		<Layout>
@@ -16,19 +18,31 @@ export default function Homepage() {
 			</Title>
 			<Row>
 				<Col span={12}>
-					<Statistic title="Total Cryptocurrencies" value={5} />
+					<Statistic title="Total Cryptocurrencies" value={globalStats.total} />
 				</Col>
 				<Col span={12}>
-					<Statistic title="Total Exchanges" value={5} />
+					<Statistic
+						title="Total Exchanges"
+						value={millify(globalStats.totalExchanges)}
+					/>
 				</Col>
 				<Col span={12}>
-					<Statistic title="Total Market Cap" value={5} />
+					<Statistic
+						title="Total Market Cap"
+						value={millify(globalStats.totalMarketCap)}
+					/>
 				</Col>
 				<Col span={12}>
-					<Statistic title="Total 24h Volume" value={5} />
+					<Statistic
+						title="Total 24h Volume"
+						value={millify(globalStats.total24hVolume)}
+					/>
 				</Col>
 				<Col span={12}>
-					<Statistic title="Total Markets" value={5} />
+					<Statistic
+						title="Total Markets"
+						value={millify(globalStats.totalMarkets)}
+					/>
 				</Col>
 			</Row>
 		</Layout>
